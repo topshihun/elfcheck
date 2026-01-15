b6d38d6e9635cd1bfe9e9289efedc360c27ab2c9
Wangyan <topshihun@foxmail.com>
2026-01-15 21:34:44 +0800

    refactor: utils print

--- src/utils.zig
+++ src/utils.zig
@@ -5,34 +5,23 @@ const COLOR_RESET = "\x1b[0m";
 const FORE_COLOR_RED = "\x1b[31m";
 
 pub fn print(comptime fmt: []const u8, args: anytype) void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer {
-        const deinit_status = gpa.deinit();
-
-        if (deinit_status == .leak) @panic("memory leak");
-    }
-
-    const out_buffer = std.fmt.allocPrint(allocator, fmt, args) catch {
-        @panic("alloc error");
-    };
-    defer allocator.free(out_buffer);
-    const std_out = std.fs.File.stdout();
-    std_out.writeAll(out_buffer) catch |err| {
-        @panic(@errorName(err));
+    var stdout = std.fs.File.stdout().writer(&.{}).interface;
+    stdout.print(fmt, args) catch |e| {
+        std.debug.print("err:{any}\n", .{e});
+        std.debug.print("Should print:\n", .{});
+        std.debug.print(fmt, args);
+        unreachable;
     };
 }
 
 pub fn printNoArgs(comptime str: []const u8) void {
-    std.fs.File.stdout().writeAll(str) catch |err| {
-        @panic(@errorName(err));
-    };
+    print(str, .{});
 }
 
 pub fn print_error(comptime fmt: []const u8, args: anytype) void {
-    print(FORE_COLOR_RED, .{});
+    printNoArgs(FORE_COLOR_RED);
     print(fmt, args);
-    print(COLOR_RESET, .{});
+    printNoArgs(COLOR_RESET);
 }
 
 pub fn strEql(s1: []const u8, s2: []const u8) bool {
